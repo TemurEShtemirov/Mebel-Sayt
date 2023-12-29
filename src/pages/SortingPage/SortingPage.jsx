b44dab7b0167c1816917122e38584acb45f742a0
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-29 12:44:26 +0500

    added pages and added bootstrap

---
+++ src/pages/SortingPage/SortingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SortingPage = () => {
+  return (
+    <div>SortingPage</div>
+  )
+}
---
+++ src/pages/WorkerTask/WorkerTask.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const WorkerTask = () => {
+  return (
+    <div>WorkerTask</div>
+  )
+}
